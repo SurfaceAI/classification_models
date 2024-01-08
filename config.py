c575754d75970c08e59f1a28b141dcc44884d32f
estherweig <estherweigmann@posteo.de>
2024-01-08 11:52:39 +0100

    added config

--- config.py
+++
@@ -1,2 +0,0 @@
-rateke_surface_type_model_path = r"C:\Users\esthe\Documents\GitHub\classification_models\Road Surface Classification Rateke adapted\01Surface Type"
-training_data_path = '/Users/edith/HTW Cloud/SHARED/SurfaceAI/data/mapillary_images/training_data'
