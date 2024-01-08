5b26069b14804b31935646d0b49ff348b6b698ee
estherweig <estherweigmann@posteo.de>
2024-01-08 11:56:55 +0100

    update config

--- Road Surface Classification Rateke adapted/01Surface Type/config.py
+++ Road Surface Classification Rateke adapted/01Surface Type/config.py
@@ -1,2 +1,2 @@
-rateke_surface_type_model_path = r"C:\Users\esthe\Documents\GitHub\classification_models\Road Surface Classification Rateke adapted\01Surface Type"
-training_data_path = r"C:\Users\esthe\Documents\GitHub\classification_models\Road Surface Classification Rateke adapted\01Surface Type\training_data"
+rateke_surface_type_model_path = r"\home\alexandra\surfaceai\classification_models\Road Surface Classification Rateke adapted\01Surface Type"
+training_data_path = r"\home\alexandra\surfaceai\classification_models\Road Surface Classification Rateke adapted\01Surface Type\training_data"
