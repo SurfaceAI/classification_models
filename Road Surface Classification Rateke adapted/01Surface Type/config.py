34bf6af1a8bc79753cb9c6bba6be2b918661bcbf
AlexandraKapp <alexandra.kapp2@gmail.com>
2024-01-08 12:19:47 +0100

    specify gpu kernel

--- Road Surface Classification Rateke adapted/01Surface Type/config.py
+++ Road Surface Classification Rateke adapted/01Surface Type/config.py
@@ -1,2 +1,4 @@
+gpu_kernel = 1
+
 rateke_surface_type_model_path = r"C:\Users\esthe\Documents\GitHub\classification_models\Road Surface Classification Rateke adapted\01Surface Type"
 training_data_path = r"C:\Users\esthe\Documents\GitHub\classification_models\Road Surface Classification Rateke adapted\01Surface Type\training_data"
