f794eb01a99114cb2e1f1b9f89151024ac77ff35
AlexandraKapp <alexandra.kapp2@gmail.com>
2024-01-09 15:57:51 +0100

    change root_dir path

--- utils/general_config.py
+++ utils/general_config.py
@@ -1,7 +1,6 @@
 from pathlib import Path
-
 gpu_kernel = 1
 
-cwd = Path.cwd()
-training_data_path = cwd / 'data' / 'training'
-save_path = cwd.parent / 'models'
+ROOT_DIR = Path(__file__).parent.parent
+training_data_path = ROOT_DIR / 'data' / 'training'
+save_path = ROOT_DIR / 'models'
