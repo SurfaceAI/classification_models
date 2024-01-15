a057f174c08395c403504d9421a43d0e7dcc2fbd
AlexandraKapp <alexandra.kapp2@gmail.com>
2024-01-15 15:33:02 +0100

    test

--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -1,20 +1,20 @@
-from experiments.training_config import rateke_cnn_default, vgg16_default
+from experiments.config import rateke_cnn_default, vgg16_default
 from src import train_model_esther
 
-
 def get_config(config_name):
-    # call general config
-    config = general_config.config
+            # call general config
+            config = general_config.config
 
-    # call model config
-    config_model = rateke_cnn_default.config
+            # call model config
+            config_model = rateke_cnn_default.config
 
-    # append and replace by config_model
-
-    return config
+            # append and replace by config_model
 
 
-# config = vgg16_default.config
+            return config 
+
+
+#config = vgg16_default.config
 config.learning_rate = 0.0001
 
-train_model_esther.config_and_train_model(get_config("rakete_cnn"))
+train_model_esther.config_and_train_model(get_config('rakete_cnn'))
