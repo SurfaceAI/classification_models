95f656f718dcc62f83a8d9562c6989bd59a9c8fd
Edith Hoffmann <EdithHoffmann@protonmail.com>
2024-04-02 11:23:55 +0200

    experiments updated

---
+++ experiments/quality_efficientnet_fixed_train.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('.')
+
+from src import constants as const
+from src.models import training
+from experiments.config import train_config, global_config
+
+surfaces = [
+        const.ASPHALT,
+        const.CONCRETE,
+        const.PAVING_STONES,
+        const.SETT,
+        const.UNPAVED,
+        ]
+
+for surface in surfaces:
+    config = {
+            **train_config.effnet_quality_regression_params,
+            'epochs': 8,
+            'learning_rate': 0.0005,
+            'level': f'{surface}',
+            'dataset': f'V9/annotated/{surface}',
+            'selected_classes': global_config.global_config.get("selected_classes")[f'{surface}']
+            }
+    print(config)
+    training.run_training(config=config)
