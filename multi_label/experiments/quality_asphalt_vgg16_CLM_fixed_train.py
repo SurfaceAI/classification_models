5b9b70bc573ffdd2359fada96f08e382d541a759
estherweig <estherweigmann@posteo.de>
2024-07-01 14:25:40 +0200

    created script for CLM asphalt training

---
+++ multi_label/experiments/quality_asphalt_vgg16_CLM_fixed_train.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('.')
+
+from src.models import training
+from experiments.config import train_config
+
+config = train_config.vgg16_asphalt_CLM_params
+
+training.run_training(config=config)
+
+# for max_class_size in [50, 100, 200, 400, 600, None]:
+#     config["max_class_size"] = max_class_size
+#     training.run_training(config=config)
