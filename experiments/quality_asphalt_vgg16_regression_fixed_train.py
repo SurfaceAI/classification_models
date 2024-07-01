94fcb1cd56c18b54b84909418c79d23b96ef6d10
estherweig <estherweigmann@posteo.de>
2024-07-01 14:25:10 +0200

    training without different values for max_class_size

--- experiments/quality_asphalt_vgg16_regression_fixed_train.py
+++ experiments/quality_asphalt_vgg16_regression_fixed_train.py
@@ -5,7 +5,8 @@ from src.models import training
 from experiments.config import train_config
 
 config = train_config.vgg16_asphalt_regression_params
+training.run_training(config=config)
 
-for max_class_size in [50, 100, 200, 400, 600, None]:
-    config["max_class_size"] = max_class_size
-    training.run_training(config=config)
+# for max_class_size in [50, 100, 200, 400, 600, None]:
+#     config["max_class_size"] = max_class_size
+#     training.run_training(config=config)
