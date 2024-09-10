07b9d8a16055e31a5254a4d4e5d6270b3d60d088
estherweig <estherweigmann@posteo.de>
2024-09-10 15:34:56 +0200

    created quality sweep scripts

--- multi_label/experiments/ordinal_asphalt_sweep_clm.py
+++ multi_label/experiments/ordinal_asphalt_sweep_clm.py
@@ -4,4 +4,4 @@ sys.path.append('.')
 from src.models import training
 from experiments.config import train_config
 
-training.run_training(config=train_config.ordinal_methods_sweep_params, is_sweep=True)
+training.run_training(config=train_config.asphalt_quality_clm_sweep_params, is_sweep=True)
