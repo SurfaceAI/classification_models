8eb2c1bbc73b96944d125d20d4839ab6a4411243
estherweig <estherweigmann@posteo.de>
2024-10-20 18:15:42 +0200

    changed fixed training script asphalt

--- multi_label/experiments/ordinal_asphalt_fixed.py
+++ multi_label/experiments/ordinal_asphalt_fixed.py
@@ -4,4 +4,4 @@ sys.path.append('.')
 from src.models import training
 from experiments.config import train_config
 
-training.run_training(config=train_config.asphalt_quality_corn_fixed_params, is_sweep=False)
+training.run_training(config=train_config.asphalt_quality_fixed_params, is_sweep=False)
